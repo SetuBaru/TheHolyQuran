fb30a5f111d4ffb3357f5d2fb772802fcefdb8ae
Abubakr Osman <78774159+SetuBaru@users.noreply.github.com>
2024-03-30 05:18:47 +0200

    Update start.sh
    macos compatible

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,10 +6,24 @@ python3 -m pip install --upgrade pip
 # Download the pre-trained models (AraVec cbow_100_twitter for now)
 cd backend/api/semantic/models || exit
 
-if [[ -z "$(ls -A -I 'README.md' -I '.gitignore' ./)" ]]; then
-	wget https://bakrianoo.ewr1.vultrobjects.com/aravec/full_grams_cbow_100_twitter.zip
-	unzip full_grams_cbow_100_twitter.zip
+if [[ -z "$(ls -A | grep -vE 'README.md|\.gitignore')" ]]; then
+  if command -v curl &>/dev/null; then
+	  curl -O https://bakrianoo.ewr1.vultrobjects.com/aravec/full_grams_cbow_100_twitter.zip
+  else
+      echo "Error: curl not found. Please install curl."
+      exit 1
+  fi
+
+  # Check if unzip is available
+  if command -v unzip &>/dev/null; then
+      unzip full_grams_cbow_100_twitter.zip
+  else
+      echo "Error: unzip not found. Please install unzip."
+      exit 1
+  fi
 fi
+
+
 cd ../../../../
 
 # Also, KSUCCA full_cbow model is light to use, however, you will need to configure it manually in the code
